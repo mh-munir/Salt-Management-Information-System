cb5a661aea6db4001be48073fd8998a6195ffb00
MH Munir <mhmunir914794@gmail.com>
2026-05-03 14:58:15 +0600

    added project

--- components/Table.tsx
+++ components/Table.tsx
@@ -8,3 +8,24 @@ type Props = {
 };
 
 export default function Table({ columns, data }: Props) {
+  return (
+    <table className="w-full">
+      <thead>
+        <tr>
+          {columns.map((col) => (
+            <th key={col}>{col}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((row, idx) => (
+          <tr key={idx}>
+            {columns.map((col) => (
+              <td key={col}>{row[col]}</td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
